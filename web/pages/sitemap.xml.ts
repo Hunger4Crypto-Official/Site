d0e84a41e958b3776accbba7599cade88c28143d
Hunger4Crypto <hunger4crypto@gmail.com>
2025-09-16 12:07:23 -0700

    Update sitemap.xml.ts

--- web/pages/sitemap.xml.ts
+++ web/pages/sitemap.xml.ts
@@ -1,18 +1,21 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getAllArticleSlugs } from "@/lib/content";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getAllArticleSlugs } from "../lib/content";
 
-function generateSiteMap(baseUrl: string, slugs: string[]) {
-  const urls = [`${baseUrl}/`, `${baseUrl}/articles/`, ...slugs.map(s => `${baseUrl}/articles/${s}`)];
-  return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(u => `<url><loc>${u}</loc></url>`).join('\n')}
-</urlset>`;
-}
+const BASE = process.env.NEXT_PUBLIC_SITE_URL || "https://example.com";
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com';
-  const sm = generateSiteMap(base, getAllArticleSlugs());
-  res.setHeader('Content-Type', 'application/xml');
-  res.write(sm);
-  res.end();
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const slugs = getAllArticleSlugs();
+
+  const urls = [
+    `<url><loc>${BASE}/</loc><priority>1.0</priority></url>`,
+    `<url><loc>${BASE}/articles</loc><priority>0.8</priority></url>`,
+    ...slugs.map(
+      (s) => `<url><loc>${BASE}/articles/${s}</loc><priority>0.6</priority></url>`
+    )
+  ].join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+
+  res.setHeader("Content-Type", "application/xml");
+  res.send(xml);
 }
