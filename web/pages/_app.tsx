a5e62202ed3773af90720a0a60ccf92431884bff
Hunger4Crypto <hunger4crypto@gmail.com>
2025-09-16 18:49:36 -0700

    Update _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,5 @@
-// web/pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles.css"; // make sure this file exists at web/styles.css
+import "../styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
