9042207f414676e54f02833d1275a6d863773826
Hunger4Crypto <hunger4crypto@gmail.com>
2025-09-17 23:03:56 -0700

    Add files via upload

---
+++ shared/index.js
@@ -0,0 +1,9 @@
+// Main exports for @h4c/shared package
+
+export * from './constants.js';
+export * from './utils.js';
+export * from './criteria.js';
+
+// Version info
+export const version = '1.0.0';
+export const name = '@h4c/shared';
