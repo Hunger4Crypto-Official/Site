1280cfe7f3f0e003473f1b808e9bf05e3b86e755
Hunger4Crypto <hunger4crypto@gmail.com>
2025-09-18 20:07:29 -0700

    Update _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from "next/app";
-import "../styles.css";
+// web/pages/_app.tsx
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function H4CApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
